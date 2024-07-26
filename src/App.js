4d75ed9c3f5ceaeee7beb51ddb88dbe17dc4e5d7
Hithaishi <hithaishi952000@gmail.com>
2024-07-26 21:31:28 +0530

    user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import {Link,Routes,Route} from "react-router-dom"
+import { useEffect } from "react";
+import axios from "./config/axios";
+import { useAuth } from "./context/authContext";
 import Register from "./components/userRegisteration/registerForm"
 import Home from "./components/userRegisteration/Home"
 import VerifyOTP from "./components/userRegisteration/verifyOTP"
 import Login from "./components/userRegisteration/Login"
+import Account from "./components/userRegisteration/account"
+import ForgetPassword from "./components/userRegisteration/forgetPassword";
+import ResetPassword from "./components/userRegisteration/resetPassword";
 
 function App() {
+  const {user,dispatch} = useAuth()
+  useEffect(()=>{
+    if(localStorage.getItem('token')){
+      (async()=>{
+        const response = await axios.get('/user/account',{
+          headers:{
+            Authorization: localStorage.getItem('token')
+          }
+        })
+        dispatch({type:'LOGIN',payload:{account:response.data}})
+      })()
+    }
+  },[])
+  
   return(
-    <div>
+    <div >
       <h1>PetBuddy</h1>
-      <Link to="/">Home</Link>|
-      <Link to="/register">Register</Link>|
-      <Link to="/login">Login</Link>
+      <Link to='/'>Home</Link>
+      {!user.isLoggedIn ?(
+        <>
+        |<Link to='/register'>Register</Link>|
+        <Link to='/login'>Login</Link>
+        </>
+      ):(
+        <>
+        |<Link to='/account'>Account</Link>|
+       
+        <Link to='/' onClick={()=>{
+          localStorage.removeItem('token')
+          dispatch({type:'LOGOUT'})
+        }}>Logout</Link>
+        </>
+      )}
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/verify-OTP" element={<VerifyOTP/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/forget-password" element={<ForgetPassword/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/account" element={<Account/>} />
       </Routes>
     </div>
   )
   
 }
+
 
 export default App;
--- src/components/userRegisteration/Login.js
+++ src/components/userRegisteration/Login.js
@@ -1,29 +1,28 @@
-import {useState} from "react"
-import axios from "../../config/axios"
-import {useFormik} from "formik"
-import * as yup from "yup"
-import {useNavigate} from "react-router-dom"
-import { toast } from 'react-toastify'
-import {useAuth} from "../../context/authContext"
+import axios from "../../config/axios";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../../context/authContext";
+import {Link,Routes,Route} from "react-router-dom"
 
+const loginValidationSchema = yup.object({
+    email: yup.string().required("Email is required").email("Invalid email format"),
+    password: yup.string().required("Password is required").min(8, "Password must be at least 8 characters").max(128, "Password cannot exceed 128 characters")
+});
 
-const loginValidationsSchema= yup.object({
-    email: yup.string().required("Email is required").email("Invalid Email Format"),
-    password: yup.string().required("Password is required").min(8,"Password must be 8 Characters").max(16,"Password must be 16 Characters")
-})
-
-export default function Login(){
-
-    const {dispatch}=useAuth()
-    const navigate=useNavigate()
-
+export default function Login() {
+    const { dispatch } = useAuth();
+    const navigate = useNavigate();
+    const [serverErrors, setServerErrors] = useState('');
+    
     const formik = useFormik({
-        initialValues:{
-            email:"",
-            password:""
+        initialValues: {
+            email: "",
+            password: ""
         },
-        validationOnChange : false,
-        validationSchema: loginValidationsSchema,
+        validateOnChange: false,
+        validationSchema: loginValidationSchema,
         onSubmit: async (values) => {
             try {
                 const response = await axios.post('/user/login', values);
@@ -39,45 +38,45 @@ export default function Login(){
                 
                 console.log('userResponse', userResponse.data);
                 dispatch({ type: "LOGIN", payload: { account: userResponse.data } });
-                toast.success('Login Success', {
-                    autoClose: 1000,
-                    onClose: () => {
-                        navigate('/account');
-                    }
-                })
+                navigate("/");
             } catch (err) {
                 console.log(err);
-                
+                setServerErrors(err.response.data.errors);
             }
         }
     });
-
     
-
-    return(
+    return (
         <div>
-            <h1>Login </h1>
+            <h1>Login Here..</h1>
+            {serverErrors && <b>{serverErrors}</b>}
             <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="email">Enter Email</label> <br/>
-                    <input 
-                        type="text"
-                        value={formik.values.email}
-                        name="email"
-                        onChange={formik.handleChange}
-                        id="email"
-                    /> <br />
-                     <label htmlFor="password">Enter Password</label><br/>
-                    <input 
-                        type="password"
-                        value={formik.values.password}
-                        name="password"
-                        onChange={formik.handleChange}
-                        id="password"
-                    /><br/ >
-                    <input type="submit" />
-                    
-               
+                <label htmlFor="email">Enter Email</label> <br />
+                <input
+                    type="text"
+                    value={formik.values.email}
+                    name="email"
+                    onChange={formik.handleChange}
+                    id="email"
+                />
+                {formik.errors.email && <div>{formik.errors.email}</div>}
+                <br />
+                
+                <label htmlFor="password">Enter Password</label> <br/>
+                <input
+                    type="password"
+                    value={formik.values.password}
+                    name="password" 
+                    onChange={formik.handleChange}
+                    id="password"
+                />
+                {formik.errors.password && <div>{formik.errors.password}</div>}
+                <br />
+                
+                <input type="submit" />
             </form>
+            <Link to="/register">Create Account</Link> | 
+            <Link to="/forget-password">forget password</Link> |
         </div>
-    )
+    );
 }
